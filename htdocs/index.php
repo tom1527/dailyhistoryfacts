eebab7d7bfccb38f30b71a25ea2237cdf5c7f798
tom1527 <twbradley@hotmail.com>
2021-06-08 13:07:18 +0100

    Accepting indexd

---
+++ htdocs/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+    <head>Something</head>
+    <body>
+    Or other
+    <br>
+    <form action="Hello.php" method="get">
+    Name: <input type=text" name="name">
+    <input type="submit">
+    </form>
+    </head>
+</html>
