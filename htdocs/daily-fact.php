60a88ce4f8b797aa0f6d8e474f90bd47ed375a0e
tom1527 <twbradley@hotmail.com>
2021-06-08 15:04:36 +0100

    Edited for general clarity and laid foundation for daily-fact.php

--- htdocs/daily-fact.php
+++ htdocs/daily-fact.php
@@ -8,6 +8,26 @@
         <h1>Interesting Fact</h1>
     </body>
 
+<?php
+
+require ("connect-to-database.php");
+global $dbc;
+echo date("jS F");
+?> <br> <?php
+echo "test";
+?> <br> <?php
+$currentDate = date("Y-m-d");
+echo $currentDate;
+?> <br> <?php
+$result = mysqli_query($dbc, "SELECT * FROM facts WHERE date = '$currentDate';");
+$factInfo = mysqli_fetch_assoc($result);
+if ($row) {
+    echo $factInfo['fact'];
+} else {
+    echo "error";
+}
+
+?>
 
 
 
