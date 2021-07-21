86a16dc37350cc466be07c5c193f91539e8f204b
tom1527 <twbradley@hotmail.com>
2021-07-21 15:32:15 +0100

    Fixed a bug wherein archive.php would lose its active class upon form submission.

--- htdocs/navBar.php
+++ htdocs/navBar.php
@@ -3,9 +3,9 @@
 <div class="navbar">
         <nav>
             <ul>
-                <li><a <?php echo checkActiveClass("daily-fact.php"); ?>href="daily-fact.php">Daily Fact</a></li>
-                <li><a <?php echo checkActiveClass("archive.php"); ?> href="archive.php">Archive</a></li>
-                <li><a <?php echo checkActiveClass("about.php"); ?> href="about.php">About</a></li>
+                <li><a <?php echo checkActiveClass("daily-fact"); ?>href="daily-fact.php">Daily Fact</a></li>
+                <li><a <?php echo checkActiveClass("archive"); ?> href="archive.php">Archive</a></li>
+                <li><a <?php echo checkActiveClass("about"); ?> href="about.php">About</a></li>
             </ul>    
         </nav>
     </div>
@@ -14,6 +14,8 @@
     function checkActiveClass($value) {
         $page = $_SERVER['REQUEST_URI'];
         $page = ltrim($page, "/");
+        $page = substr($page, 0 , strpos($page, "."));
+
         if($page == $value) {
             return "class='active'";
         }
