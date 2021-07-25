c547af996829b1d7b41edba4e2b1339d51a36b38
tom1527 <twbradley@hotmail.com>
2021-07-25 23:31:14 +0100

    Implemented 3 classes to cover MVC model and OOP style code.

--- htdocs/classes/ResultsDisplayer.class.php
+++ htdocs/classes/ResultsDisplayer.class.php
@@ -4,21 +4,28 @@
 class ResultsDisplayer {
 
     private DatabaseSearcher $searcher;
+    private SearchTermExtractor $extractor;
     
 
-    public function __construct($searcher) {
+    public function __construct($searcher, $extractor) {
         $this->searcher = $searcher;
+        $this->extractor = $extractor;
     }
     
     public function displayResults(): void {
         
         $results = $this->searcher->getSearchResults();
+        $totalResults = $this->searcher->countSearchResults();
+        $searchTerm = $this->extractor->extractSearchTerms();
+        
 
+        echo "<div>Search of \"$searchTerm[0]\" returned $totalResults.</div>";
         foreach ($results as $i => $result) {
             $resultNumber = $i + 1;
             echo "<div class='archiveResults'>Result: $resultNumber";
             echo "<h4 class='result'>$result[day]/$result[month]</h4>";
             echo "<p class='result'>$result[fact]</p>";
+    
             if($result['link']) {
                 echo "<p>Click <a href='$result[link]' target='blank'>here</a> to learn more about this event.</p>";
             }
