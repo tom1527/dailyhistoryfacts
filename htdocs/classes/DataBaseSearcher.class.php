2c0467ca6232a8b5f7d97bd602de5d6b4f7156e5
tom1527 <twbradley@hotmail.com>
2021-07-26 13:59:15 +0100

    Decoupled classes from one another.

--- htdocs/classes/DataBaseSearcher.class.php
+++ htdocs/classes/DataBaseSearcher.class.php
@@ -2,32 +2,31 @@
 
 <?php
 class DatabaseSearcher extends DataBaseConn {
-    private $searchTerm;
-    private $sortBy;
-    private $pageNo;
-    private $limitBy;
 
-    public function __construct(string $searchTerm, string $sortBy, int $pageNo, int $limitBy)
-    {
-        $this->searchTerm = $searchTerm;
-        $this->sortBy = $sortBy;
-        $this->pageNo = $pageNo;
-        $this->limitBy = $limitBy;
+    public $searchTerm; 
+    public $sortBy; 
+    public $pageNo;
+    public $limitBy;
+
+    public function __construct(array $searchTerms) {
+        $this->searchTerms = $searchTerms;
     }
 
     public function getSearchResults(): array {
+        $searchTerms = $this->searchTerms;
         $sql = "SELECT * FROM `facts` WHERE `fact` LIKE CONCAT('%', ?, '%')";
         $stmt = $this->connect()->prepare($sql);
-        $stmt->execute([$this->searchTerm]);
+        $stmt->execute([$searchTerms['0']]);
 
         $results = $stmt->fetchAll();
         return $results;        
     }
 
     public function countSearchResults(): int {
+        $searchTerms = $this->searchTerms;
         $sql = "SELECT * FROM `facts` WHERE `fact` LIKE CONCAT('%', ?, '%')";
         $stmt = $this->connect()->prepare($sql);
-        $stmt->execute([$this->searchTerm]);
+        $stmt->execute([$searchTerms['0']]);
         
         $results = $stmt->fetchAll();
         $totalNumberOfSearchResults = count($results);
