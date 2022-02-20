6e9056fed8f2606038832fe8de7e10dd5463a4ad
tom <twbradley@hotmail.com>
2022-02-20 17:23:27 +0000

    Update host name for rds

--- lib/classes/DatabaseConn.class.php
+++ lib/classes/DatabaseConn.class.php
@@ -1,6 +1,6 @@
 <?php
     class DataBaseConn {
-        private $host = "db";
+        private $host = "database-cluster.cluster-cb1bkopanp3s.eu-west-2.rds.amazonaws.com";
         private $user = "root";
         private $psw = "KgdS8Ilbf8J3";
         private $dbname = "interestingfacts";
