c554160f00f25ad37e273952735f4e4f37187730
tom <twbradley@hotmail.com>
2022-02-25 01:53:06 +0000

    Set charset to avoid encoding issues when deployed

--- lib/classes/DatabaseConn.class.php
+++ lib/classes/DatabaseConn.class.php
@@ -1,12 +1,14 @@
 <?php
     class DataBaseConn {
-        private $host = "database-cluster.cluster-cb1bkopanp3s.eu-west-2.rds.amazonaws.com";
+        //private $host = "database-cluster.cluster-cb1bkopanp3s.eu-west-2.rds.amazonaws.com";
+        private $host = "db";
+
         private $user = "root";
         private $psw = "KgdS8Ilbf8J3";
         private $dbname = "interestingfacts";
 
         protected function connect(): PDO {
-            $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->dbname;
+            $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->dbname . ";charset=utf8";
             $pdo = new PDO($dsn, $this->user, $this->psw);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             return $pdo;
