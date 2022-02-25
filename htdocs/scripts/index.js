a1d4764d762462589130b0fb7c08df356f601381
tom <twbradley@hotmail.com>
2022-02-25 01:53:30 +0000

    Create switch fact button and choose date calander

--- htdocs/scripts/index.js
+++ htdocs/scripts/index.js
@@ -1,9 +1,25 @@
-var date = new Date();
-var month = date.getMonth() + 1;
-var monthLong = date.toLocaleString('default', { month: 'long' });
-var day = date.getDate();
-var nth = function(value) {
-if (value > 3 && value < 21) return 'th';
+setDate();
+
+function setDate(monthLong, month, day){
+    if(!month || !day){
+        var date = new Date();
+        var month = date.getMonth() + 1;
+        var monthLong = date.toLocaleString('default', { month: 'long' });
+        var day = date.getDate();
+    } 
+
+    nth = setOrdinals(day);
+
+    today = "Today's date is: " + monthLong + " " + day + nth;
+    document.getElementById("today").innerHTML = today;
+    factDate = "On " + monthLong + " " + day + nth + " in the past... ";
+    getFact(month, day);
+}
+
+function setOrdinals(value){
+    if (value > 3 && value < 21) {
+        return 'th';
+    }
     switch (value % 10) {
         case 1:  return "st";
         case 2:  return "nd";
@@ -11,32 +27,32 @@ if (value > 3 && value < 21) return 'th';
         default: return "th";
     }
 }
-today = "Today's date is: " + monthLong + " " + day + nth(day);
-factDate = "On " + monthLong + " " + day + nth(day) + " in the past... ";
-document.getElementById("today").innerHTML = today;
-    //document.getElementById("changeFact").innerHTML = "hi";
-let url = "daily-fact.php?month=" + month + "&day=" + day;
 
-const dailyFactDate = new Request(url, {
-    credentials: 'include',
-    type: 'GET',
-    method: 'GET',
-    headers: {
-        'Content-Type': 'text/html',
-    },
-    mode: 'same-origin',
-    cache: 'default',
-});
+function getFact(month, day) {
+    let url = "daily-fact.php?month=" + month + "&day=" + day;
 
-let div=document.getElementById("daily-fact");
+    const dailyFactDate = new Request(url, {
+        credentials: 'include',
+        type: 'GET',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'text/html',
+        },
+        mode: 'same-origin',
+        cache: 'default',
+    });
 
-fetch(dailyFactDate)
-    .then(response => response.text())
-    .then(html =>  {
-        div.innerHTML = html;
-        document.getElementById("firstFactDate").innerHTML = factDate;
-    } 
-);
+    
+    let div=document.getElementById("daily-fact");
+    
+    fetch(dailyFactDate)
+        .then(response => response.text())
+        .then(html =>  {
+            div.innerHTML = html;
+            document.getElementById("firstFactDate").innerHTML = factDate;
+        } 
+    );
+}
 
 function changeFact() {
     var first = document.getElementById("first");
@@ -52,7 +68,20 @@ function changeFact() {
         last.style.display = "none";
     }
     document.getElementById("lastFactDate").innerHTML = factDate;
-
-
-
 }
+
+$(function() {
+    $('.DateTextBox.NoYear').datepicker({
+        showAnim: "",
+        dateFormat: 'MM/m/d',
+        changeYear: false,
+        onSelect: function(date) {
+            multipleDates = date.split("/");
+            const monthLong = multipleDates[0];
+            const month = multipleDates[1];
+            const day = multipleDates[2];
+            let url = "daily-fact.php?month=" + month + "&day=" + day;
+            setDate(monthLong, month, day);
+        },
+    });    
+});
