b1e35dfe01f94934af12675aa3b0b6f48fefaa75
tom <twbradley@hotmail.com>
2022-03-18 21:31:18 +0000

    Fix API issues for daily fact

--- htdocs/api.php
+++ htdocs/api.php
@@ -7,35 +7,27 @@ $day = $_GET['day'];
 $month = $_GET['month'];
 
 $factInfo = setJSONResponse($search, $day, $month);
+ob_clean();
 
-if(empty($factInfo) {
-    $factInfo = "No results matching those terms";
+if(empty($factInfo)) {
+    echo "No results matching those terms.";
 } else { 
-    ob_clean();
-    $json_response = json_encode($results);
-    echo $json_response;   
+    $json_response = json_encode($factInfo); 
+    echo $json_response;
 }
 
 function setJSONResponse($search, $day, $month) {
     if($search){
-        if(empty($search)) {
-            $error = "Invalid search parameter";
-            return;
-        }
         $searchTermExtractor = new SearchTermExtractor($search, '---', 1, 10);
         $searchTerms = $searchTermExtractor->extractSearchTerms();
-        $dataBaseSearcher = new DatabaseSearcher($searchTerms);
-        $results = $dataBaseSearcher->getSearchResults();
+        $dataBaseSearcher = new DatabaseSearcher();
+        $results = $dataBaseSearcher->getSearchResults($searchTerms);
         return $results;
     }
 
     if($day && $month) {
-        if(empty($day) || emtpty($month)) {
-            $error = "Invalid day or month parameters";
-            return;
-        }
-        $databaseSearcher = new DatabaseSearcher($value);
-        $dailyFactInfo = $databaseSearcher->returnDailyFact($currentDay, $currentMonth);
+        $databaseSearcher = new DatabaseSearcher();
+        $dailyFactInfo = $databaseSearcher->returnDailyFact($day, $month);
         return $dailyFactInfo;
     }
 }
