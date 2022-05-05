4cea66febd23c52a0c03219335a9ea7dfc103a6d
tom <twbradley@hotmail.com>
2022-05-05 18:41:29 +0100

    Move host to environment

--- lib/classes/DatabaseConn.class.php
+++ lib/classes/DatabaseConn.class.php
@@ -1,13 +1,13 @@
 <?php
     class DataBaseConn {
-        private $host = "database";
-        private $user = "root";
-        private $psw = "KgdS8Ilbf8J3";
-        private $dbname = "interestingfacts";
-
         protected function connect(): PDO {
-            $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->dbname . ";charset=utf8";
-            $pdo = new PDO($dsn, $this->user, $this->psw);
+            $host = getEnv('ENVIRONMENT') == 'LIVE' ? "127.0.0.1" : 'database';
+            $user = "root";
+            $psw = "KgdS8Ilbf8J3";
+            $dbname = "interestingfacts";
+
+            $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
+            $pdo = new PDO($dsn, $user, $psw);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             return $pdo;
         }
