5c6b88f022ae2a681b5901c6d793e9cc72fd3500
tom <twbradley@hotmail.com>
2022-08-08 23:30:44 +0100

    Set up first tests

--- tests/SearchTermExtractorTest.php
+++ tests/SearchTermExtractorTest.php
@@ -1,15 +1,24 @@
 <?php declare(strict_types=1);
-
 use PHPUnit\Framework\TestCase;
 
-final class MyTest extends TestCase
+class BuildSearchParametersTest extends TestCase
 {
 
-    public function SearchTermExtractorReturns(): void
+    public function testBuildSearchParametersHasKeys(): void
     {
-
+        $searchTerms = SearchParameterBuilder::buildSearchParameters("treaty", "dateDes", 1, 10);
+        $this->assertArrayHasKey("searchTerm", $searchTerms, "\$searchTerms lacks the \"searTerm\" key.");
+        $this->assertArrayHasKey("sortBy", $searchTerms, "\$searchTerms lacks the \"searTerm\" key.");
+        $this->assertArrayHasKey("pageNo", $searchTerms, "\$searchTerms lacks the \"searTerm\" key.");
+        $this->assertArrayHasKey("maxResults", $searchTerms, "\$searchTerms lacks the \"searTerm\" key.");
     }
 
+    public function testBuildSearchParametersSubsets(): void
+    {
+        $searchTerms = SearchParameterBuilder::buildSearchParameters("treaty", "dateDes", 1, 10);
+        $subset = array("searchTerm" => "treaty");
+        $this->assertArraySubset($subset, $searchTerms, true, "searchTerms lacks subset as a subset");
+    }
 }
 
 
