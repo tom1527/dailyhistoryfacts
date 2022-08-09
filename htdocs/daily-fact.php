ad5030893410bc98d3835815e51c086ee47b1fe6
tom <twbradley@hotmail.com>
2022-08-09 20:45:47 +0100

    Whitespace for clarity

--- htdocs/daily-fact.php
+++ htdocs/daily-fact.php
@@ -12,14 +12,19 @@ if (empty($_GET['day'])) {
     http_response_code(400);
     $errorTitle = 'Sorry, we couldn\'t load a fact.';
     $errorDescription = "Error: request missing 'day' parameter.";
+
 } else if (empty($_GET['month'])) {
     http_response_code(400);
     $errorTitle = 'Sorry, we couldn\'t load a fact.';
     $errorDescription = "Error: request missing 'month' parameter.";
+
 } else {
+
     $pdo = DatabaseConn::connect();
     $databaseSearcher = new DatabaseSearcher($pdo);
+
     $dailyFactInfo = $databaseSearcher->returnDailyFact($_GET['day'], $_GET['month']);
+
     if (empty($dailyFactInfo)) {
         http_response_code(404);
         $errorDescription = "Error: the entry for this date is most likely empty. This will be fixed as the website is updated.";
